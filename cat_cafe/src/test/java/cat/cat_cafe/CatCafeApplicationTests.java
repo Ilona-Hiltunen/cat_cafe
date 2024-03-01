3f26208a052521ca56800659c21b2bfb709bd3cd
Ilona <ilona.hiltunen@myy.haaga-helia.fi>
2024-03-01 22:29:35 +0200

    created a new project

---
+++ cat_cafe/src/test/java/cat/cat_cafe/CatCafeApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.cat_cafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatCafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
