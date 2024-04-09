54070ab3997505d1b013996b02e947fd64f8cb2d
Ilona <ilona.hiltunen@myy.haaga-helia.fi>
2024-04-10 01:29:46 +0300

    fixes

--- cat_cafe/src/main/java/cat/cat_cafe/CatConfig.java
+++ cat_cafe/src/main/java/cat/cat_cafe/CatConfig.java
@@ -5,10 +5,11 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
 import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -18,6 +19,7 @@ import cat.cat_cafe.web.AppUserService;
 
 @Configuration
 @EnableMethodSecurity(securedEnabled = true)
+@EnableWebMvc
 public class CatConfig {
 
     @Autowired
